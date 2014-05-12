5105b4de694e1f5da789657f3f972565e5cacc3a
Sean Eshbaugh <seaneshbaugh@gmail.com>
2014-05-12 03:44:50 -0500

    Reverted Commander 2013 common symbol. Oops.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,24 +2,26 @@ class StatsController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-    @total_cards_in_collections = Collection.where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0', current_user.id).sum(:quantity)
+    @total_cards_in_collections = Collection.joins(:card_list).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `card_lists`.`have` = 1', current_user.id).sum(:quantity)
 
-    @unique_cards_in_collections = Collection.where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0', current_user.id).count
+    @unique_cards_in_collections = Collection.joins(:card_list).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `card_lists`.`have` = 1', current_user.id).select('distinct `collections`.`card_id`').count
 
-    @card_blocks = CardBlock.includes(:card_sets)
+    @card_sets = CardSet.includes(:card_block => :card_block_type).order('`card_block_types`.`id`, `card_blocks`.`id`, `card_sets`.`release_date` ASC')
 
-    @card_sets = @card_blocks.map { |card_block| card_block.card_sets }.flatten
-
-    #@collections = Collection.includes(:card).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0', current_user.id).order('`collections`.`quantity` ASC')
-
-    %w(White Blue Black Red Green).each do |color|
-      instance_variable_set "@number_of_#{color.downcase}", Collection.joins(:card).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `cards`.`colors` = ?', current_user.id, color).sum(:quantity)
-    end
-
-    @number_of_multi = Collection.joins(:card).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `cards`.`colors` LIKE "%;%"', current_user.id).sum(:quantity)
-
-    @number_of_colorless = Collection.joins(:card).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `cards`.`colors` = ""', current_user.id).sum(:quantity)
-
-    @number_of_land = Collection.joins(:card).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `cards`.`card_types` LIKE "%Land%"', current_user.id).sum(:quantity)
+    #@card_blocks = CardBlock.includes(:card_sets)
+    #
+    #@card_sets = @card_blocks.map { |card_block| card_block.card_sets }.flatten
+    #
+    ##@collections = Collection.includes(:card).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0', current_user.id).order('`collections`.`quantity` ASC')
+    #
+    #%w(White Blue Black Red Green).each do |color|
+    #  instance_variable_set "@number_of_#{color.downcase}", Collection.joins(:card).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `cards`.`colors` = ?', current_user.id, color).sum(:quantity)
+    #end
+    #
+    #@number_of_multi = Collection.joins(:card).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `cards`.`colors` LIKE "%;%"', current_user.id).sum(:quantity)
+    #
+    #@number_of_colorless = Collection.joins(:card).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `cards`.`colors` = ""', current_user.id).sum(:quantity)
+    #
+    #@number_of_land = Collection.joins(:card).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `cards`.`card_types` LIKE "%Land%"', current_user.id).sum(:quantity)
   end
 end
