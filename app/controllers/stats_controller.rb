a92e8342da7de8d9462dfbf6246020c1175dba32
Sean Eshbaugh <seaneshbaugh@gmail.com>
2014-05-12 13:56:08 -0500

    Better stats page.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -8,6 +8,28 @@ class StatsController < ApplicationController
 
     @card_sets = CardSet.includes(:card_block => :card_block_type).order('`card_block_types`.`id`, `card_blocks`.`id`, `card_sets`.`release_date` ASC')
 
+    @card_set_stats = @card_sets.map do |card_set|
+      card_in_set = card_set.cards.count
+
+      unique_cards = Collection.joins(:card_list, :card).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `card_lists`.`have` = 1 AND `cards`.`card_set_id` = ?', current_user.id, card_set.id).select('distinct `collections`.`card_id`').count
+
+      total_cards = Collection.joins(:card_list, :card).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `card_lists`.`have` = 1 AND `cards`.`card_set_id` = ?', current_user.id, card_set.id).sum(:quantity)
+
+      if card_in_set > 0
+        percent_collected = ((unique_cards.to_f / card_in_set.to_f) * 100.0).to_i
+      else
+        percent_collected = 0
+      end
+
+      if unique_cards == card_in_set
+        rarity = :rare
+      else
+        rarity = :common
+      end
+
+      { :card_set => card_set, :card_in_set => card_in_set, :unique_cards => unique_cards, :total_cards => total_cards, :percent_collected => percent_collected, :rarity => rarity }
+    end
+
     #@card_blocks = CardBlock.includes(:card_sets)
     #
     #@card_sets = @card_blocks.map { |card_block| card_block.card_sets }.flatten
