47aa30e858a9c2369c6f7f46063240c8095646f0
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-04-11 18:02:31 -0500

    Updated some controller stuff.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,49 +1,43 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: %i[show edit update confirm_destroy destroy]
+
   before_filter :authenticate_user!
 
-  def show
-    @account = current_user
-  end
+  def show; end
 
-  def edit
-    @account = current_user
-  end
+  def edit; end
 
   def update
-    @account = current_user
+    if @account.update(account_params)
+      bypass_sign_in(@account)
 
-    if params[:account]
-      params[:account].delete(:role)
+      flash[:success] = t('.success')
 
-      params[:account].delete(:receive_sign_up_alerts)
-
-      params[:account].delete(:receive_contact_alerts)
-    end
-
-    if @account.update_attributes(params[:account])
-      sign_in(@account, :bypass => true)
-
-      flash[:success] = t('messages.accounts.updated')
-
-      redirect_to edit_account_url
+      redirect_to edit_account_url, status: :see_other
     else
-      flash.now[:error] = @account.errors.full_messages.uniq.join('. ') + '.'
+      flash.now[:error] = helpers.error_messages_for(@account)
 
-      render 'edit'
+      render 'edit', status: :unprocessable_entity
     end
   end
 
-  def confirm_delete
-    @account = current_user
-  end
+  def confirm_destroy; end
 
   def destroy
-    @account = current_user
-
     @account.destroy
 
-    flash[:success] = 'Your account has successfully been deleted.'
+    flash[:success] = t('.success')
 
-    redirect_to root_url
+    redirect_to root_url, status: :see_other
+  end
+
+  private
+
+  def set_account
+    @account = current_user
+  end
+
+  def account_params
+    params.require(:account).permit(:username, :email, :password, :password_confirmation, :first_name, :last_name)
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
   def index
     @new_user = User.new
