03919fd4c3bf28124e34dc65a0dbf50b2ed3f7bb
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-04-14 14:09:54 -0500

    Added configuration for permitted registration params for devise.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@
 class ApplicationController < ActionController::Base
   include Pundit
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protect_from_forgery
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username email])
+  end
 
   private
 
