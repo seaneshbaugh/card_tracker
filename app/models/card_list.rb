3ef06c91ae1bf80d028b4e411579e552396082de
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-04-16 16:40:45 -0500

    Updated card list model.

--- app/models/card_list.rb
+++ app/models/card_list.rb
@@ -1,50 +1,27 @@
+# frozen_string_literal: true
+
 class CardList < ActiveRecord::Base
-#  attr_accessible :user_id, :name, :slug, :have, :order, :default
+  DEFAULT_LISTS = [
+    { name: 'Have', have: true, order: 0, default: true },
+    { name: 'Want', have: false, order: 1, default: false },
+    { name: 'Have (foil)', have: true, order: 2, default: false },
+    { name: 'Want (foil)', have: false, order: 3, default: false }
+  ].freeze
 
   belongs_to :user
+  has_many :collections, dependent: :destroy
+  has_many :cards, through: :collections
 
-  has_many :collections
-  has_many :cards, :through => :collections
+  validates :user_id, presence: true
+  validates :name, presence: true, uniqueness: { scope: :user_id }
+  validates :slug, presence: true, uniqueness: { scope: :user_id }
+  validates :have, inclusion: { in: [true, false] }
+  validates :order, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 2_147_483_647 }, presence: true
+  validates :default, inclusion: { in: [true, false] }
 
-  validates_presence_of :user_id
-
-  validates_presence_of   :name
-  validates_uniqueness_of :name, :scope => :user_id
-
-  validates_presence_of   :slug
-  validates_uniqueness_of :slug, :scope => :user_id
-
-  validates_inclusion_of :have, :in => [true, false]
-
-  validates_numericality_of :order, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2_147_483_647
-  validates_presence_of     :order
-
-  validates_inclusion_of :default, :in => [true, false]
-
-  after_initialize do
-    if self.new_record?
-      self.name ||= ''
-      self.slug ||= ''
-
-      if self.have.nil?
-        self.have = true
-      end
-
-      self.order ||= 0
-    end
-  end
-
+  after_initialize :set_default_attribute_values, if: :new_record?
   before_validation :generate_slug
-
-  before_save do
-    if self.default && self.default_changed?
-      card_lists = CardList.where('`card_lists`.`id` != ? AND `card_lists`.`user_id` = ?', self.id, self.user_id)
-
-      card_lists.each do |card_list|
-        card_list.update_column :default, false
-      end
-    end
-  end
+  before_save :ensure_only_one_default
 
   protected
 
@@ -54,5 +31,20 @@ class CardList < ActiveRecord::Base
     else
       self.slug = CGI.unescapeHTML(Sanitize.clean(self.name)).gsub(/'|"/, '').gsub('&', 'and').squeeze(' ').parameterize
     end
+  end
+
+  private
+
+  def ensure_only_one_default
+    return unless default && default_changed?
+
+    CardList.where(user_id: user_id).where.not(id: id).update_all(default: false)
+  end
+
+  def set_default_attribute_values
+    self.name ||= ''
+    self.slug ||= ''
+    self.have = true if have.nil?
+    self.order ||= 0
   end
 end
