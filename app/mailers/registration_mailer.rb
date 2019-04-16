f29994ce78fd543b2186fecfe6e25007ba505acb
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-04-16 16:42:33 -0500

    Updated registration mailer.

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 class RegistrationMailer < ActionMailer::Base
-  default :from => 'noreply@cavesofkoilos.com'
+  default from: 'noreply@cavesofkoilos.com'
 
   def new_sign_up_message(user, new_user)
     @user = user
-
     @new_user = new_user
 
-    mail :to => @user.email, :from => 'Caves of Koilos Registration <registration@cavesofkoilos.com>', :subject => "#{@new_user.email} has created an account."
+    mail to: @user.email, subject: "#{@new_user.email} has created an account."
   end
 
   def welcome_message(new_user)
     @new_user = new_user
 
-    mail :to => @new_user.email, :from => 'Sean from Caves of Koilos <sean@cavesofkoilos.com>', :subject => 'Keeping track of your Magic: The Gathering collection just got easier!'
+    mail to: @new_user.email, subject: 'Keeping track of your Magic: The Gathering collection is about to get easier!'
   end
 end
