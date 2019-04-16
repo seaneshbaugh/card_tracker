987c4ee44f3415ef8de9886f181f21aea55955ce
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-04-16 16:38:17 -0500

    Add separate initialization for dropdowns since the defaults aren't desirable.

--- app/javascript/shared/javascripts/index.js
+++ app/javascript/shared/javascripts/index.js
@@ -1,7 +1,13 @@
 import "materialize-css/dist/js/materialize.js";
 import "./flash_messages";
 
-const completed = () => (M.AutoInit());
+const completed = () => {
+  M.AutoInit();
+
+  const dropdowns = document.querySelectorAll(".dropdown-trigger");
+
+  M.Dropdown.init(dropdowns, { alignment: "right", constrainWidth: false, coverTrigger: false });
+};
 
 if (document.readyState === "complete") {
   setTimeout(completed);
