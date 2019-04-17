120c2b23bebf3aabac8a0624fe4b908e265d3a36
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-04-16 23:37:33 -0500

    Updated collection model.

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,22 +1,20 @@
-class Collection < ActiveRecord::Base
-#  attr_accessible :card_id, :user_id, :card_list_id, :quantity
+# frozen_string_literal: true
 
+class Collection < ApplicationRecord
   belongs_to :card
   belongs_to :user
   belongs_to :card_list
 
-  validates_presence_of :card_id
+  validates :card_id, presence: true
+  validates :user_id, presence: true
+  validates :card_list_id, presence: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 2_147_483_647 }, presence: true
 
-  validates_presence_of :user_id
+  after_initialize :set_default_attribute_values, if: :new_record?
 
-  validates_presence_of :card_list_id
+  private
 
-  validates_presence_of     :quantity
-  validates_numericality_of :quantity, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2_147_483_647
-
-  after_initialize do
-    if self.new_record?
-      self.quantity ||= 0
-    end
+  def set_default_attribute_values
+    self.quantity ||= 0
   end
 end
