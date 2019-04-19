9a2131a5f9b7f59d596b416f722739cd199d6f44
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-04-19 18:23:21 -0500

    Updated test helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,41 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_group 'Policies', 'app/policies'
+  add_group 'Presenters', 'app/presenters'
+  add_group 'Serializers', 'app/serializers'
+  add_group 'Validators', 'app/validators'
+end
+
 ENV['RAILS_ENV'] = 'test'
-require File.expand_path('../../config/environment', __FILE__)
+
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
+require 'minitest/reporters'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
+mean_time_reporter_options = {
+  previous_runs_filename: Rails.root.join('tmp', 'minitest_reporters_previous_run'),
+  report_filename: Rails.root.join('tmp', 'minitest_reporters_report')
+}
 
-  # Add more helper methods to be used by all tests here...
+Minitest::Reporters.use! Minitest::Reporters::MeanTimeReporter.new(mean_time_reporter_options)
+
+module ActiveSupport
+  class TestCase
+    # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+    #
+    # Note: You'll currently still have to declare fixtures explicitly in integration tests
+    # -- they do not yet inherit this setting
+    fixtures :all
+
+    # Add more helper methods to be used by all tests here...
+  end
+end
+
+module ActionDispatch
+  class IntegrationTest
+    include Devise::Test::IntegrationHelpers
+  end
 end
