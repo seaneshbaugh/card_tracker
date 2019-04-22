160b7d4118a3378851191add2c33c48efba0e971
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-04-22 13:41:29 -0500

    Added inverse_of.

--- app/models/card_block.rb
+++ app/models/card_block.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CardBlock < ApplicationRecord
-  has_many :card_sets, -> { order(release_date: :asc) }, dependent: :restrict_with_exception
+  has_many :card_sets, -> { order(release_date: :asc) }, dependent: :restrict_with_exception, inverse_of: :card_block
 
   validates :name, presence: true, uniqueness: true
 end
