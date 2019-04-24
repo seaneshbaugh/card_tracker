651b7e79093af29982e01c20bf41af9e28fccdd4
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-04-23 23:03:31 -0500

    Updated CardType tests.

--- app/models/card_type.rb
+++ app/models/card_type.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class CardType < ApplicationRecord
-  has_many :card_typings, dependent: :restrict_with_exception
+  has_many :card_typings, dependent: :restrict_with_exception, foreign_key: :card_type_code, inverse_of: :card_type
   has_many :cards, through: :card_typings
 
-  validates :card_type_code, presence: true
-  validates :name, presence: true
+  validates :card_type_code, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
--- test/models/card_type_test.rb
+++ test/models/card_type_test.rb
@@ -1,24 +1,36 @@
 require 'test_helper'
 
 class CardTypeTest < ActiveSupport::TestCase
+  let(:card_type) { CardType.new }
+
+  describe 'associations' do
+    it 'has many card_typings' do
+      card_type.must have_many(:card_typings).dependent(:restrict_with_exception).with_foreign_key(:card_type_code).inverse_of(:card_type)
+    end
+
+    it 'has many cards through card_typings' do
+      card_type.must have_many(:cards).through(:card_typings)
+    end
+  end
+
   describe 'validations' do
     describe 'card_type_code' do
       it 'validates presence of card_type_code' do
-        card_type = CardType.new
+        card_type.must validate_presence_of(:card_type_code)
+      end
 
-        card_type.validate
-
-        card_type.errors[:card_type_code].must_include("can't be blank")
+      it 'validates uniqueness of card_type_code' do
+        card_type.must validate_uniqueness_of(:card_type_code)
       end
     end
 
     describe 'name' do
       it 'validates presence of name' do
-        card_type = CardType.new
+        card_type.must validate_presence_of(:name)
+      end
 
-        card_type.validate
-
-        card_type.errors[:name].must_include("can't be blank")
+      it 'validates uniqueness of name' do
+        card_type.must validate_uniqueness_of(:name)
       end
     end
   end
