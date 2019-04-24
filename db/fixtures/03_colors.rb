f9432da7294c1c44dae5b09e98ae7a56cfd0a663
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-04-23 23:03:46 -0500

    Fixed Color seeds.

--- db/fixtures/03_colors.rb
+++ db/fixtures/03_colors.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
-Color.seed(:color_code) do |t|
+Color.seed(:color_code) do |s|
   s.color_code = 'W'
   s.name = 'White'
 end
 
-Color.seed(:color_code) do |t|
+Color.seed(:color_code) do |s|
   s.color_code = 'U'
   s.name = 'Blue'
 end
 
-Color.seed(:color_code) do |t|
+Color.seed(:color_code) do |s|
   s.color_code = 'B'
   s.name = 'Black'
 end
 
-Color.seed(:color_code) do |t|
+Color.seed(:color_code) do |s|
   s.color_code = 'R'
   s.name = 'Red'
 end
 
-Color.seed(:color_code) do |t|
+Color.seed(:color_code) do |s|
   s.color_code = 'G'
   s.name = 'Green'
 end
