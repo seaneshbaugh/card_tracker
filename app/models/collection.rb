05852a244181fadc6de49941cd4a37cc81adb5e8
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-05-14 15:21:50 -0500

    Added display_order scope and clamp_quantity before validation callback. Not sure about checking if quantity is an Integer. Leaving that in for now so instead of raising an exception we get a validation error instead.

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -5,15 +5,29 @@ class Collection < ApplicationRecord
   belongs_to :user, inverse_of: :collections
   belongs_to :card_list, inverse_of: :collections
 
+  scope :display_order, -> do
+    includes(card: { card_set: %i[card_block card_set_type] })
+      .merge(CardSet.display_order)
+      .merge(Card.display_order)
+      .references(:card, :card_blocks, :card_sets, :card_set_types)
+  end
+
+  scope :quantity_greater_than_zero, -> { where(Collection.arel_table[:quantity].gt(0)) }
+
   validates :quantity, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 2_147_483_647 }, presence: true
 
   after_initialize :set_default_attribute_values, if: :new_record?
+  before_validation :clamp_quantity
 
   def cards?
     quantity.positive?
   end
 
   private
+
+  def clamp_quantity
+    self.quantity = quantity.clamp(0, 2_147_483_647) if quantity.is_a?(Integer)
+  end
 
   def set_default_attribute_values
     self.quantity ||= 0
