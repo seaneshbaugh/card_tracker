f0e4a9c04ee3987d024f9b63630c270c66b3fca5
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-05-24 12:10:27 -0500

    Fixed inverse_of for CardPart has many through relationships.

--- app/models/card_part.rb
+++ app/models/card_part.rb
@@ -6,10 +6,10 @@ class CardPart < ApplicationRecord
   has_many :card_part_super_typings, dependent: :restrict_with_exception, inverse_of: :card_part
   has_many :card_part_typings, dependent: :restrict_with_exception, inverse_of: :card_part
   has_many :card_part_sub_typings, dependent: :restrict_with_exception, inverse_of: :card_part
-  has_many :colors, through: :card_part_colorings, foreign_key: :color_code, inverse_of: :card_part
-  has_many :card_super_types, through: :card_part_super_typings, foreign_key: :card_super_type_code, inverse_of: :card_part
-  has_many :card_types, through: :card_part_typings, foreign_key: :card_type_code, inverse_of: :card_part
-  has_many :card_sub_types, through: :card_part_sub_typings, foreign_key: :card_sub_type_code, inverse_of: :card_part
+  has_many :colors, through: :card_part_colorings, foreign_key: :color_code, inverse_of: :card_parts
+  has_many :card_super_types, through: :card_part_super_typings, foreign_key: :card_super_type_code, inverse_of: :card_parts
+  has_many :card_types, through: :card_part_typings, foreign_key: :card_type_code, inverse_of: :card_parts
+  has_many :card_sub_types, through: :card_part_sub_typings, foreign_key: :card_sub_type_code, inverse_of: :card_parts
 
   delegate :card_set, to: :card, allow_nil: true
 
