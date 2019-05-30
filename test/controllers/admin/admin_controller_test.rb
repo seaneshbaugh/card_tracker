08c345c9b6b8edd567aa0e253e5e09f8652ef0d3
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-05-30 15:37:25 -0500

    Added one admin users controller test.

--- test/controllers/admin/admin_controller_test.rb
+++ test/controllers/admin/admin_controller_test.rb
@@ -4,5 +4,21 @@ require 'test_helper'
 
 module Admin
   class AdminControllerTest < ActionDispatch::IntegrationTest
+    let(:admin) { users(:user1) }
+
+    setup do
+      admin.add_role(:user)
+      admin.add_role(:admin)
+    end
+
+    describe '#index' do
+      it 'should show the index page' do
+        sign_in(admin)
+
+        get admin_root_url
+
+        assert_response :ok
+      end
+    end
   end
 end
