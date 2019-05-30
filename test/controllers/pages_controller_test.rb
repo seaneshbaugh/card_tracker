e7772f5a6609a769801cc4ad79ac42c9f6974a17
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-05-30 15:36:44 -0500

    Updated descriptions.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 # Commenting this out because it is SLOW. I have no idea why.
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  describe 'index' do
+  describe '#index' do
     it 'should show the index page' do
       get root_url
 
@@ -12,9 +12,9 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  describe 'show' do
+  describe '#show' do
     context 'valid pages' do
-      it 'shows valid pages' do
+      it 'should show valid pages' do
         %w[about privacy-policy terms-and-conditions].each do |page|
           get page_url(id: page)
 
@@ -24,7 +24,7 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     end
 
     context 'invalid pages' do
-      it 'returns a 404' do
+      it 'should return a 404' do
         proc { get page_url(id: 'does-not-exist') }.must_raise(ActionController::RoutingError)
       end
     end
