799dc08a6c68c53626aecf46f905c65c9d7999bd
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-06-23 19:46:05 -0500

    I18nized stat views.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,20 +4,15 @@ class StatsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @total_cards_in_collections = Collection.joins(:card_list).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `card_lists`.`have` = 1', current_user.id).sum(:quantity)
-
-    @unique_cards_in_collections = Collection.joins(:card_list).where('`collections`.`user_id` = ? AND `collections`.`quantity` <> 0 AND `card_lists`.`have` = 1', current_user.id).select('distinct `collections`.`card_id`').count
-
-    @card_sets = CardSet.includes(card_block: :card_block_type).order('`card_block_types`.`id`, `card_blocks`.`id`, `card_sets`.`release_date` ASC')
-
+    @total_cards_in_collections = Collection.joins(:card_list).where(user_id: current_user.id, card_lists: { have: true }).quantity_greater_than_zero.sum(:quantity)
+    @unique_cards_in_collections = Collection.joins(:card_list).where(user_id: current_user.id, card_lists: { have: true }).quantity_greater_than_zero.select(:card_id).count
+    @card_sets = CardSet.display_order
     @card_set_stats = @card_sets.map { |card_set| CardSetStat.new(card_set, current_user) }
   end
 
   def show
     @card_set = CardSet.where(slug: params[:id]).first
-
     @card_set_stat = CardSetStat.new(@card_set, current_user)
-
-    @card_lists = CardList.where(user_id: current_user.id, have: true).order('`card_lists`.`order` ASC')
+    @card_lists = CardList.where(user_id: current_user.id, have: true).display_order
   end
 end
