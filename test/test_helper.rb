794411057425479252a69835bba624ebb4c23870
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-09-26 11:01:10 -0500

    Updated some test stuff.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,7 +12,7 @@ end
 
 ENV['RAILS_ENV'] = 'test'
 
-require File.expand_path('../config/environment', __dir__)
+require_relative '../config/environment'
 require 'rails/test_help'
 require 'minitest/reporters'
 
@@ -33,10 +33,7 @@ module ActiveSupport
       alias context describe
     end
 
-    # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-    #
-    # Note: You'll currently still have to declare fixtures explicitly in integration tests
-    # -- they do not yet inherit this setting
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
