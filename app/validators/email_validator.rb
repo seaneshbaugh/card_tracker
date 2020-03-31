1fa8673255f8dc7cc8e33e2dd764a8571b6efa3e
Sean Eshbaugh <seaneshbaugh@gmail.com>
2020-03-31 14:28:59 -0500

    Filter obvious spam email addresses.

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -3,8 +3,14 @@
 # Taken from https://github.com/gitlabhq/gitlabhq/blob/master/app/validators/devise_email_validator.rb
 class EmailValidator < ActiveModel::EachValidator
   DEFAULT_OPTIONS = {
-    regexp: Devise.email_regexp
+    regexp: Devise.email_regexp,
+    not_spam: true
   }.freeze
+
+  SPAM_DOMAINS = [
+    'mail.ru',
+    'yandex.com'
+  ]
 
   def initialize(options)
     options.reverse_merge!(DEFAULT_OPTIONS)
@@ -18,5 +24,13 @@ class EmailValidator < ActiveModel::EachValidator
     return if options[:allow_blank] && value.blank?
 
     record.errors.add(attribute, options[:message] || :invalid) unless value =~ options[:regexp]
+
+    record.errors.add(attribute, options[:messsge] || :invalid) if options[:not_spam] && is_spam?(value)
+  end
+
+  private
+
+  def is_spam?(value)
+    SPAM_DOMAINS.any? { |spam_domain| value.ends_with?("@#{spam_domain}") }
   end
 end
